acd0e5121c17eae91d432530fbb324e1c0c0f52c
Anders Rundgren <anders.rundgren.net@gmail.com>
2026-02-21 09:47:49 +0100

    Update largefile.py

--- test/largefile.py
+++ test/largefile.py
@@ -1,28 +1,48 @@
+# largefile.py
+
 from org.webpki.cbor import CBOR
 from cryptography.hazmat.primitives import hashes
 import http.client
 
+FILE_KEY = CBOR.String("file")
+SHA256_KEY = CBOR.String("sha256")
+CBOR_MAX_LENGTH = 1000
+CHUNK_SIZE = 1000
+
+# Initiate the hash method
+digest = hashes.Hash(hashes.SHA256())
+
+# Perform an HTTP request
 conn = http.client.HTTPSConnection("cyberphone.github.io")
-conn.request("GET", "/javaapi/app-notes/large-payloads/payload.bin")
+conn.request("GET", "/CBOR.py/doc/app-notes/large-payloads/payload.bin")
 response = conn.getresponse()
+
+# Show the response status
 print(response.status, response.reason)
-decoder = CBOR.init_decoder(response, CBOR.SEQUENCE_MODE, 10000)
+
+# Decode CBOR using the received stream handle
+decoder = CBOR.init_decoder(response, CBOR.SEQUENCE_MODE, CBOR_MAX_LENGTH)
 metadata = decoder.decode_with_options()
+
+# Print CBOR object
 print(metadata)
 print("CBOR bytes: " + str(decoder.get_byte_count()))
-digest = hashes.Hash(hashes.SHA256())
-count = 0
+
+# The rest of the stream is supposed to be the file.
+# Read data in moderately-sized chunks.
+total_bytes = 0
 while True:
-    buffer = response.read(1000)
-    if buffer:
-        digest.update(buffer)
-        count += len(buffer)
+    chunk = response.read(CHUNK_SIZE)
+    if chunk:
+        # Hash the chunk.
+        digest.update(chunk)
+        total_bytes += len(chunk)
+        ###################################################
+        # Store the chunk in an application-specific way. #
+        ###################################################
     else: break
-if digest.finalize() != metadata.get(CBOR.String("sha256")).get_bytes():
+if digest.finalize() != metadata.get(SHA256_KEY).get_bytes():
     print("Failed at SHA256")
 else:
-    print("Succefully received file: {:s} ({:n}))"
-          .format(metadata.get(CBOR.String('file')).get_string(), count))
-
-
-
+    print("Succesfully received: {:s} ({:n}))"
+          .format(metadata.get(FILE_KEY).get_string(), total_bytes))
